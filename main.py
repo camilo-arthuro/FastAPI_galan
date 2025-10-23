c215e60636456492a7236c9b695acb1db0031643
camilo-arthuro <camiloarthuro94@gmail.com>
2025-10-23 19:55:42 +0200

    New endpoints and ProductPartial class created

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI, Depends
-#from pydantic import BaseModel
 from sqlmodel import SQLModel, create_engine, Session, select
 from dotenv import load_dotenv
-from models.product import Product, ProductInfo, ProductResponse
+from .models.product import Product, ProductInfo, ProductResponse
 import os
 
 load_dotenv()
@@ -19,36 +18,40 @@ def get_db():
 
 app = FastAPI()
 
-@app.post("/api/users", response_model=dict, tags=["CREATE"])
+@app.post("/api/product", response_model=dict, tags=["CREATE"])
 async def create_product(new_product: ProductInfo, db:Session = Depends(get_db)):
     insert_product = Product.model_validate(new_product)
     db.add(insert_product)
     db.commit()
     return {"msg":"Inserció correcta"}
 
-@app.get("/api/users/{id}", response_model=ProductResponse, tags=["READ by ID"])
-async def get_user_id(id: int, db: Session = Depends(get_db)):
+@app.get("/api/product/{id}", response_model=ProductResponse, tags=["READ by ID"])
+async def get_product_id(id: int, db: Session = Depends(get_db)):
     product_by_id=select(Product).where(Product.id == id)
     result= db.exec(product_by_id).first()
     return ProductResponse.model_validate(result)
-'''
-@app.get("/api/users", response_model=dict)
-async def get_all_users():
-    numbers = range(len(users_list))
-    users_dic = dict(zip(numbers, users_list))
-    return users_dic
 
-@app.put("/api/users/{id}", response_model=dict)
-async def update_user(id: int, updated_user: str):
-    users_dic = {}
-    users_list[id] = updated_user
-    users_dic[id] = users_list[id]
-    return users_dic
+@app.get("/api/product", response_model=ProductResponse, tags=["READ ALL"])
+async def get_all_products(db: Session = Depends(get_db)):
+    #Sensible data: supplier_email and supplier_phone
+    all_products = db.exec(select(Product)).all()
+    return [ProductResponse.model_validate(product) for product in all_products]
 
-@app.delete("/api/usuaris/{id}", response_model=dict)
-async def delete_user(id: int):
-    users_list.pop(id)
-    numbers = range(len(users_list))
-    users_dic = dict(zip(numbers, users_list))
-    return users_dic 
-'''
+@app.get("/api/product/{name}", response_model=ProductResponse, tags=["READ by NAME"])
+async def get_products_by_name(name: str, db: Session = Depends(get_db)):
+    products_by_name = select(Product).where(Product.name == name)
+    products_list = db.exec(products_by_name).all()
+    return [ProductResponse.model_validate(product) for product in products_list]
+
+@app.delete("/api/product/{id}", response_model=dict, tags=["DELETE"])
+async def delete_product(id: int, db: Session = Depends(get_db)):
+    query = select(Product).where(Product.id == id)
+    delete_product = db.exec(query).first()
+    db.delete(delete_product)
+    db.commit()
+    return {"msg":"l’eliminació ha sigut exitosa"}
+
+@app.get("/api/product/{id}/partial", response_model=ProductPartial, tags=["READ PARTIALLY"])
+async def get_product_partially(id: int, db: Session = Depends(get_db)):
+    query = select()
+    return
--- models/product.py
+++ models/product.py
@@ -3,21 +3,25 @@ from sqlmodel import SQLModel, Field
 class Product(SQLModel, table = True):
     id: int = Field(default=None, primary_key=True)
     name: str
-    description: str
     price: float
     stock: int
     supplier_email: str
+    supplier_phone: str
 
 class ProductInfo(SQLModel):
     name: str
-    description: str
     price: float
     stock: int
     supplier_email: str
+    supplier_phone: str
 
 class ProductResponse(SQLModel):
     id: int
     name: str
-    description: str
+    price: float
+    stock: int
+
+class ProductPartial(SQLModel):
+    name: str
     price: float
     stock: int
